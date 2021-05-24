0891e6a203eb6bd3a5a59194c33f6831761c9d45
minh-bao <mbnguyen@hotmail.fr>
2021-05-24 16:36:49 +0200

    add quagga component

--- pages/index.vue
+++ pages/index.vue
@@ -12,9 +12,7 @@
       <div class="text-md-center">
         <img src="https://static.openfoodfacts.org/images/misc/openfoodfacts-logo-fr-178x150.png">
       </div>
-      <div>
-        <v-quagga :onDetected="logIt" :readerSize="readerSize" :readerTypes="['ean_reader']"></v-quagga>
-      </div>
+      <quagga />
       <v-alert
         :value="alert"
         type="error"
@@ -45,23 +43,19 @@
 
 <script>
 import axios from 'axios'
+import Quaggas from '~/components/Quaggas.vue'
 
 export default {
+  components: {
+    quagga: Quaggas
+  },
   data () {
     return {
       code: '',
-      alert: false,
-      readerSize: {
-        width: 640,
-        height: 480
-      },
-      detecteds: []
+      alert: false
     }
   },
   methods: {
-    logIt (data) {
-      console.log('detected', data)
-    },
     submit () {
       if (this.code !== 0 && !isNaN(this.code)) {
         axios.get(`https://fr.openfoodfacts.org/api/v0/produit/${this.code}.json`)
