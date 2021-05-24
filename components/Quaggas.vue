4eb9123ec7728e00ae756e56752144f757ec67aa
minh-bao <mbnguyen@hotmail.fr>
2021-05-24 16:35:57 +0200

    create component to open stream

---
+++ components/Quaggas.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <StreamBarcodeReader
+      @decode="onDecode"
+      @loaded="onLoaded"
+    />
+  </div>
+</template>
+
+<script>
+import { StreamBarcodeReader } from 'vue-barcode-reader'
+
+export default {
+  components: {
+    StreamBarcodeReader: StreamBarcodeReader
+  }
+}
+
+</script>
